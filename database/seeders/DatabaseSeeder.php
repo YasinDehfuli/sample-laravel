87d12716b85775d3476d0bb291f9684a784e4744
Nisay <110921275+YasinDehfuli@users.noreply.github.com>
2023-04-16 14:23:23 +0330

    Add Database Seeder Laravel

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
 use App\Models\Post;
+use Database\Factories\CategoryFactory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,6 +17,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+
+        Category::factory(5)->create();
 
         $this->call([
             UserTableSeeder::class,
