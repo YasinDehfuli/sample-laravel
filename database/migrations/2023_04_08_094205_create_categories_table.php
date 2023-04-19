26f98d734d363dca915ab37160c24036184fa7e5
Nisay <110921275+YasinDehfuli@users.noreply.github.com>
2023-04-19 21:38:02 +0330

    Add Cat Table Factory

---
+++ database/migrations/2023_04_08_094205_create_categories_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('title',128);
+            $table->string('slug',64)->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+};
