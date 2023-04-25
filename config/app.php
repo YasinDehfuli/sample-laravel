f291e96a6f3d616fca36fab933e044d9e8a82254
Nisay <110921275+YasinDehfuli@users.noreply.github.com>
2023-04-25 22:00:57 +0330

    Update app.php

--- config/app.php
+++ config/app.php
@@ -17,6 +17,8 @@ return [
 
     'name' => env('APP_NAME', 'Laravel'),
 
+    'under' => env('UNDER_CONSTRUCTION', false),
+
     /*
     |--------------------------------------------------------------------------
     | Application Environment
